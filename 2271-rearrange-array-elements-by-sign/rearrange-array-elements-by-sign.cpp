697bf2f01e09d40054ac1499d26aaef5efa98201
theja <animator.bhanu@gmail.com>
2026-03-08 19:29:00 +0530

    Time: 13 ms (17.15%) | Memory: 145 MB (7.98%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>pos;
+        vector<int>neg;
+        vector<int>c;
+        for(int i = 0;i<n;i++){
+            if (nums[i]>0)
+                pos.push_back(nums[i]);
+            else 
+            neg.push_back(nums[i]);
+
+        }
+        for (int i = 0; i < n/2; i++) {
+        c.push_back(pos[i]);
+        c.push_back(neg[i]);
+
+    }return c;
+
+        }
+};
